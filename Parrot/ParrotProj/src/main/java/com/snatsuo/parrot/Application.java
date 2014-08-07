c52366a195e11f08c20b198d1858025ad3d12398
shirashiki <silvio.hirashiki@gmail.com>
2014-08-07 17:02:34 -0400

    up

--- Parrot/ParrotProj/src/main/java/com/snatsuo/parrot/Application.java
+++ Parrot/ParrotProj/src/main/java/com/snatsuo/parrot/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 //Tell Spring to automatically inject any dependencies that are marked in
