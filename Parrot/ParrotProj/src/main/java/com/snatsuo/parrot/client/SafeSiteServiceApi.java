77d46d84dbcac6409f9cbc2695f11e5e9c2accb3
shirashiki <silvio.hirashiki@gmail.com>
2014-08-07 16:33:58 -0400

    committing project

---
+++ Parrot/ParrotProj/src/main/java/com/snatsuo/parrot/client/SafeSiteServiceApi.java
@@ -0,0 +1,10 @@
+package com.snatsuo.parrot.client;
+
+
+public interface SafeSiteServiceApi {
+
+	// The path where we expect the VideoSvc to live
+	public static final String SAFESITE_SVC_PATH = "/safesite";
+
+
+}
---
+++ Parrot/ParrotProj/src/main/java/com/snatsuo/parrot/controller/SafeSiteService.java
@@ -0,0 +1,9 @@
+package com.snatsuo.parrot.controller;
+
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class SafeSiteService {
+
+}
