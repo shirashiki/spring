f6376e0617cebc0021342d3c05a739293144589a
shirashiki <silvio.hirashiki@gmail.com>
2014-08-28 11:47:04 -0400

    removed project

--- Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/BadRequestException.java
+++
@@ -1,8 +0,0 @@
-package org.magnum.mobilecloud.video;
-
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = org.springframework.http.HttpStatus.BAD_REQUEST)
-public class BadRequestException extends RuntimeException {
-
-}
